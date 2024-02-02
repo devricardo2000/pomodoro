9229f92f9d4a47a5748c2c9cd74c2bc485db3b34
Ricardo Bernardes <devricardo2000@gmail.com>
2024-02-02 19:46:42 +0000

    adding fuctions

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const timer = {
     shortBreak: 10,
     longBreak:30, 
     longBreakInterval: 4, 
+    sessions: 0,
 };
 
 const modeButtons = document.querySelector('#js-mode-buttons');
@@ -14,6 +15,7 @@ function handleMode(event) {
   if (!mode) return;
 
   switchMode(mode);
+  stopTimer();
 }
 
 function switchMode(mode) {
@@ -35,4 +37,84 @@ function switchMode(mode) {
   
     updateClock();
   }
+  function updateClock() {
+    const { remainingTime } = timer;
+    const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+    const seconds = `${remainingTime.seconds}`.padStart(2, '0');
   
+    const min = document.getElementById('js-minutes');
+    const sec = document.getElementById('js-seconds');
+    min.textContent = minutes;
+    sec.textContent = seconds;
+  }  
+let interval;
+
+function startTimer() {
+    let { total } = timer.remainingTime;
+    const endTime = Date.parse(new Date()) + total * 1000;
+    
+    if (timer.mode === 'pomodoro') timer.sessions++;
+    
+    mainButton.dataset.action = 'stop';
+    mainButton.textContent = 'stop';
+    mainButton.classList.add('active');
+  
+    interval = setInterval(function() {
+      timer.remainingTime = getRemainingTime(endTime);
+      updateClock();
+  
+      total = timer.remainingTime.total;
+      if (total <= 0) {
+        clearInterval(interval);
+        switch (timer.mode) {
+            case 'pomodoro':
+              if (timer.sessions % timer.longBreakInterval === 0) {
+                switchMode('longBreak');
+              } else {
+                switchMode('shortBreak');
+              }
+              break;
+            default:
+              switchMode('pomodoro');
+          }
+    
+          startTimer();
+      }
+    }, 1000);
+  }
+
+  function getRemainingTime(endTime) {
+    const currentTime = Date.parse(new Date());
+    const difference = endTime - currentTime;
+  
+    const total = Number.parseInt(difference / 1000, 10);
+    const minutes = Number.parseInt((total / 60) % 60, 10);
+    const seconds = Number.parseInt(total % 60, 10);
+  
+    return {
+      total,
+      minutes,
+      seconds,
+    };
+  }
+  const mainButton = document.getElementById('js-btn');
+mainButton.addEventListener('click', () => {
+  const { action } = mainButton.dataset;
+  if (action === 'start') {
+    startTimer();
+  }
+ else {
+    stopTimer();
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+    switchMode('pomodoro');
+  });
+
+  function stopTimer() {
+    clearInterval(interval);
+  
+    mainButton.dataset.action = 'start';
+    mainButton.textContent = 'start';
+    mainButton.classList.remove('active');
+  }
